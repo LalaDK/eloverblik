f3419c10fa23c389bbfae8414aa04d89e72b8318
Mads Eckardt <mads.eckardt@gmail.com>
2022-06-16 22:00:28 +0200

    func

---
+++ backend/func.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+
+async function retrieveShortLivedToken(api_token) {
+  await axios.get(
+    'https://api.eloverblik.dk/customerapi/api/token', {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${api_token}`
+    }
+  }).then((response) => {
+  }, (ajax) => {
+  })
+}
+
+retrieveShortLivedToken()
