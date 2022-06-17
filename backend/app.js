08f740361968d24de9920befd40f06d68b8aa9f0
Mads Dam Hvidbjerg Eckardt <maeck@eg.dk>
2022-06-17 13:56:58 +0200

    more

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,41 @@
- const express = require('express')
+const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios')
 const expressLogging = require('express-logging');
 const logger = require('logops');
+const eloverblik = require('./func.js');
 app.use(expressLogging(logger))
 
 app.get('/token', async (req, res) => {
-  logger.info("Hello world!!!")
-  // let token = req.params.token;
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblR5cGUiOiJDdXN0b21lckFQSV9SZWZyZXNoIiwidG9rZW5pZCI6ImI5YWQwMDUwLTNiNGMtNDA2MS05OWExLTczOGM2MmRiY2U5NCIsIndlYkFwcCI6WyJDdXN0b21lckFwaSIsIkN1c3RvbWVyQXBwQXBpIl0sImp0aSI6ImI5YWQwMDUwLTNiNGMtNDA2MS05OWExLTczOGM2MmRiY2U5NCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiUElEOjkyMDgtMjAwMi0yLTcyODE3MzQ2MDA0NyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dpdmVubmFtZSI6Ik1hZHMgRGFtIEh2aWRiamVyZyBFY2thcmR0IiwibG9naW5UeXBlIjoiS2V5Q2FyZCIsInBpZCI6IjkyMDgtMjAwMi0yLTcyODE3MzQ2MDA0NyIsInR5cCI6IlBPQ0VTIiwidXNlcklkIjoiMTU0NTQiLCJleHAiOjE2ODYzOTg2NjgsImlzcyI6IkVuZXJnaW5ldCIsInRva2VuTmFtZSI6IkFQSSIsImF1ZCI6IkVuZXJnaW5ldCJ9.eGM3JS9lZxlFebN_zUSG65RZbHb-ryBHYWpyWQyvaDc'
-  await axios.get('https://api.eloverblik.dk/customerapi/api/token', {
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${token}`
-    }
-  }).then((response) => {
-    res.send(response.data.result)
-  }, (ajax) => {
-    res.status(500)
-    res.json({ error: 'Something went wrong.' })
-  })
+  let accessToken = await eloverblik.getAccessToken(req.query.token);
+  if(accessToken) {
+    res.json({accessToken});
+  } else {
+    res.status(500);
+    res.json({error: "Unable to fetch token."});
+  }
+})
+
+app.get('/meteringpoints', async (req, res) => {
+  let meteringpoints = await eloverblik.getMeteringPoints(req.query.token, req.query.includeAll == 'true');
+  if(meteringpoints) {
+    res.json({meteringpoints});
+  } else {
+    res.status(500);
+    res.json({error: "Unable to fetch meteringspoints."});
+  }
+})
+
+
+app.get('/getdetails', async (req, res) => {
+  let details = await eloverblik.getDetails(req.query.token, req.query.meteringpoint.toString());
+  if(details) {
+    res.json({details});
+  } else {
+    res.status(500);
+    res.json({error: "Unable to fetch details."});
+  }
 })
 
 app.listen(port, () => {
--- backend/func.js
+++ backend/func.js
@@ -1,17 +1,111 @@
-const axios = require('axios')
+const axios = require('axios');
+const fs = require('fs');
+const jwtDecode = require('jwt-decode');
+const BASE_API_URL = 'https://api.eloverblik.dk/customerapi/api';
+const ACCESS_TOKEN_FILENAME = 'access-token';
+const DEV_REFRESH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblR5cGUiOiJDdXN0b21lckFQSV9SZWZyZXNoIiwidG9rZW5pZCI6ImI5YWQwMDUwLTNiNGMtNDA2MS05OWExLTczOGM2MmRiY2U5NCIsIndlYkFwcCI6WyJDdXN0b21lckFwaSIsIkN1c3RvbWVyQXBwQXBpIl0sImp0aSI6ImI5YWQwMDUwLTNiNGMtNDA2MS05OWExLTczOGM2MmRiY2U5NCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiUElEOjkyMDgtMjAwMi0yLTcyODE3MzQ2MDA0NyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dpdmVubmFtZSI6Ik1hZHMgRGFtIEh2aWRiamVyZyBFY2thcmR0IiwibG9naW5UeXBlIjoiS2V5Q2FyZCIsInBpZCI6IjkyMDgtMjAwMi0yLTcyODE3MzQ2MDA0NyIsInR5cCI6IlBPQ0VTIiwidXNlcklkIjoiMTU0NTQiLCJleHAiOjE2ODYzOTg2NjgsImlzcyI6IkVuZXJnaW5ldCIsInRva2VuTmFtZSI6IkFQSSIsImF1ZCI6IkVuZXJnaW5ldCJ9.eGM3JS9lZxlFebN_zUSG65RZbHb-ryBHYWpyWQyvaDc'
+var DEV_ACCESS_TOKEN;
 
-async function retrieveShortLivedToken(api_token) {
-  await axios.get(
-    'https://api.eloverblik.dk/customerapi/api/token', {
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${api_token}`
-    }
-  }).then((response) => {
-    console.log(res)
-}, (res) => {
-    console.log(res)
-  })
+function jwtExpirationDate(jwt_token) {
+  let jwt = jwtDecode(jwt_token);
+  let expirationDate = new Date(0);
+  expirationDate.setSeconds(expirationDate.getSeconds() + jwt.exp);
+  return expirationDate;
 }
 
-retrieveShortLivedToken()
+function jwtExpired(jwt_token) {
+  let expirationDate = jwtExpirationDate(jwt_token);
+  return new Date() > expirationDate;
+}
+
+function writeAccessTokenToFile(access_token) {
+  fs.writeFileSync(ACCESS_TOKEN_FILENAME, access_token);
+}
+
+function readAccessTokenFromFile() {
+  try {
+    if(fs.existsSync(ACCESS_TOKEN_FILENAME)) {
+      return fs.readFileSync(ACCESS_TOKEN_FILENAME);
+    }
+  } catch(error) {
+    return null;
+  }
+}
+
+async function getAccessToken(refresh_token) {
+let access_token = readAccessTokenFromFile()
+  if(false && access_token) {
+    console.info("Access token read from file");
+    DEV_ACCESS_TOKEN = access_token;
+    return access_token;
+  }
+
+  refresh_token = refresh_token || DEV_REFRESH_TOKEN;
+  try {
+    const response = await axios.get(
+      BASE_API_URL + '/token', {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ refresh_token }`
+      }
+    });
+    DEV_ACCESS_TOKEN = response.data.result;
+    writeAccessTokenToFile(response.data.result);
+    return response.data.result;
+
+  } catch(error) {
+    throw `Could not retrieve token. ${error}
+    ${ error.response.status } ${ error.response.statusMessage }
+    ${ error.response.data }
+    `;
+  }
+}
+
+async function getMeteringPoints(access_token, includeAll) {
+  access_token = access_token || DEV_ACCESS_TOKEN;
+  try {
+    const response= await axios.get(
+      BASE_API_URL + `/meteringpoints/meteringpoints?includeAll=${ !!includeAll }`, {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ access_token }`
+      }
+    });
+    return response.data.result;
+  } catch(error) {
+    console.error(error)
+    throw `Could not retrieve metering points.
+    ${ error.response.status } ${ error.response.statusMessage }
+    ${ error.response.data}`;
+  }
+}
+
+async function getDetails(access_token, meteringPoint) {
+  access_token = access_token || DEV_ACCESS_TOKEN;
+  try {
+    const response= await axios({
+      url: BASE_API_URL + `/meteringpoints/meteringpoint/getdetails`,
+      method: 'post',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ access_token }`
+      },
+      data: {
+         "meteringPoints": {
+            "meteringPoint": [meteringPoint.toString()]
+        }
+      }
+    });
+    console.log("RESPONSE", response)
+    return response.data.result;
+  } catch(error) {
+    console.error(error)
+    throw `Could not retrieve metering points.
+    ${ error.response.status } ${ error.response.statusMessage }
+    ${ error.response.data}`;
+  }
+}
+
+module.exports = {
+  getAccessToken, getDetails, getMeteringPoints
+}
