22714f7d08ea53cdf3850555bcec01a94d634fe9
Mads Dam Hvidbjerg Eckardt <maeck@eg.dk>
2022-07-01 13:51:41 +0200

    more

---
+++ frontend/src/timeseries.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+<script>
+export default {
+
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    accessToken: {
+      type: String,
+      required: true
+    },
+    meteringPoint: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date,
+      required: true
+    },
+    aggregation: {
+      type: String,
+      required: false,
+      default: 'Year'
+    }
+  }
+}
+</script>
